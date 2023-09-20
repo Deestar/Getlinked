2ff944612ee0f57f43ada15b59ec78c75c186666
Your Name <you@example.com>
2023-09-20 19:21:16 +0100

    ..

--- src/app/components/ThirdSection.jsx
+++ src/app/components/ThirdSection.jsx
@@ -28,7 +28,6 @@ export default function ThirdSection() {
           <Image
             src={Shine}
             fill
-            className=""
             sizes="100vw"
             style={{
               objectFit: "cover",
