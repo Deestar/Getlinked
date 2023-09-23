3d7bc47aa95528b1397118b05d5ba0a50ed346ff
Your Name <you@example.com>
2023-09-23 13:00:24 +0100

    did of sponsors

--- src/app/components/Eighth.jsx
+++ src/app/components/Eighth.jsx
@@ -20,30 +20,30 @@ export default function Eighth() {
         </h3>
       </header>
       <section className=" p-1 h-max w-full flex justify-center ">
-        <div className="grid grid-cols-[auto_auto_auto] grid-rows-[auto_auto_auto] gap-y-3 gap-x-5 border border-getlink py-12 px-1 se:px-10 desk:px-16 sm:px-20 lg:px-28">
-          <div className=" relative">
-            <span className="absolute  border-2 h-0 border-getlink -bottom-2 w-full "></span>
+        <div className="grid grid-cols-[auto_auto_auto] grid-rows-[auto_auto_auto] gap-y-3 gap-x-5 border border-getlink py-12 px-1 se:px-10 desk:px-16 sm:px-20 lg:px-28 row:gap-y-6 row:gap-x-[5.25rem]">
+          <div className=" relative ">
+            <span className="absolute  border-2 h-0 border-getlink -bottom-2 w-full row:-left-[3.825rem] row:w-[calc(100%_+_58px)]"></span>
             <span className="absolute  border-2 w-0 border-getlink -right-3 h-full "></span>
             <Image
               src={Assured}
-              className=" w-14 h-14 se:w-20 se:h-20 se_lg:w-24 se_lg:h-24 desk:w-28 desk:h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 row:h-40 row:w-40 lg:h-44 lg:w-44"
+              className=" w-14 h-14 se:w-20 se:h-20 se_lg:w-24 se_lg:h-24 desk:w-28 desk:h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 row:h-40 row:w-40 lg:h-44 lg:w-44 row:-translate-x-[1.9125rem] "
               alt="assured"
             />
           </div>
           <div className="relative">
-            <span className="absolute border-2 h-0 border-getlink  -bottom-2  w-full  "></span>
+            <span className="absolute border-2 h-0 border-getlink  -bottom-2  w-full  row:-left-[3.725rem] row:w-[calc(100%_+_60px)]"></span>
             <span className="absolute  border-2 w-0 border-getlink  -right-3 h-full "></span>
             <Image
               src={Liberty}
-              className=" w-14 h-14 se:w-20 se:h-20 se_lg:w-24 se_lg:h-24 desk:w-28 desk:h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 row:h-40 row:w-40 lg:h-44 lg:w-44"
+              className=" w-14 h-14 se:w-20 se:h-20 se_lg:w-24 se_lg:h-24 desk:w-28 desk:h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 row:h-40 row:w-40 lg:h-44 lg:w-44 row:-translate-x-[1.8625rem] "
               alt="liberty"
             />
           </div>
           <div className="relative">
-            <span className="absolute border-2 h-0 border-getlink   -bottom-2 w-full"></span>
+            <span className="absolute border-2 h-0 border-getlink   -bottom-2 w-full row:-left-[3.725rem] row:w-[calc(100%_+_60px)]"></span>
             <Image
               src={Winwise}
-              className=" w-14 h-14 se:w-20 se:h-20 se_lg:w-24 se_lg:h-24 desk:w-28 desk:h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 row:h-40 row:w-40 lg:h-44 lg:w-44"
+              className=" w-14 h-14 se:w-20 se:h-20 se_lg:w-24 se_lg:h-24 desk:w-28 desk:h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 row:h-40 row:w-40 lg:h-44 lg:w-44 row:-translate-x-[1.8625rem] "
               alt="vuzual"
             />
           </div>
@@ -51,7 +51,7 @@ export default function Eighth() {
             <span className="absolute  border-2 w-0 border-getlink  -right-3 h-full"></span>
             <Image
               src={Wisper}
-              className=" w-14 h-14 se:w-20 se:h-20 se_lg:w-24 se_lg:h-24 desk:w-28 desk:h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 row:h-40 row:w-40 lg:h-44 lg:w-44"
+              className=" w-14 h-14 se:w-20 se:h-20 se_lg:w-24 se_lg:h-24 desk:w-28 desk:h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 row:h-40 row:w-40 lg:h-44 lg:w-44 row:-translate-x-[1.8625rem] "
               alt="wisper"
             />
           </div>
@@ -59,14 +59,14 @@ export default function Eighth() {
             <span className="absolute  border-2 w-0 border-getlink  -right-3 h-full"></span>
             <Image
               src={Paybox}
-              className=" w-14 h-14 se:w-20 se:h-20 se_lg:w-24 se_lg:h-24 desk:w-28 desk:h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 row:h-40 row:w-40 lg:h-44 lg:w-44"
+              className=" w-14 h-14 se:w-20 se:h-20 se_lg:w-24 se_lg:h-24 desk:w-28 desk:h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 row:h-40 row:w-40 lg:h-44 lg:w-44 row:-translate-x-[1.8625rem] "
               alt="paybox"
             />
           </div>
           <div className="relative">
             <Image
               src={Vuzual}
-              className=" w-14 h-14 se:w-20 se:h-20 se_lg:w-24 se_lg:h-24 desk:w-28 desk:h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 row:h-40 row:w-40 lg:h-44 lg:w-44"
+              className=" w-14 h-14 se:w-20 se:h-20 se_lg:w-24 se_lg:h-24 desk:w-28 desk:h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 row:h-40 row:w-40 lg:h-44 lg:w-44 row:-translate-x-[1.9125rem] "
               alt="vuzual"
             />
           </div>
